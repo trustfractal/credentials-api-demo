ece483d716be079acf616411e120eb1000a453ae
Tiago Fragoso <tiagofragoso@outlook.com>
2022-07-06 14:11:45 +0200

    Add core logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,195 +1,189 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { Contract, providers } from "ethers";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+import contractABI from "../assets/abi.json";
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+const GOERLI_CHAIN_ID = 5;
+const CONTRACT_ADDRESS = "0xe951816A54aB27Cf76c22448bEc49a2765940E18";
+const BASE_URL = "https://resource.staging.sandbox.fractal.id/credentials";
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+const getLibrary = (provider) => new providers.Web3Provider(provider);
 
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
+const Injected = new InjectedConnector({});
+const WalletConnect = new WalletConnectConnector({});
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+const message = `I authorize Defistarter (dc3aa1910acbb7ff4d22c07e43a6926adc3a81305a9355a304410048c9a91afd) to get a proof from Fractal that:
+- I passed KYC level plus
+- I am not a citizen of the following countries: Germany (DE)
+- I am not a resident of the following countries: Canada (CA), United States of America (US)`;
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
+  const web3 = useWeb3React<providers.Web3Provider>();
+  const { activate, active, account, chainId, deactivate: disconnectWallet, library } = web3;
+
+  const [messageSignatureLoading, setmessageSignatureLoading] = useState(false);
+  const [messageSignature, setMessageSignature] = useState<string|undefined>(undefined);
+  const [apiCallLoading, setApiCallLoading] = useState(false);
+  const [apiCallResponseBody, setApiCallResponseBody] = useState({});
+  const [apiCallResponseStatus, setApiCallResponsestatus] = useState<number|undefined>(undefined);
+  const [apiCallError, setApiCallError] = useState<Error | undefined>(undefined);
+  const [txLoading, setTxLoading] = useState(false);
+  const [tx, setTx] = useState<any>(undefined);
+  const [txConfirmed, setTxConfirmed] = useState(false);
+  const [txError, setTxError] = useState<Error | undefined>(undefined);
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      try {
+        await activate(Injected, undefined, true);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    connectWallet();
+  }, [])
+
+  const switchToGoerliChain = () => {
+    library.provider.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: `0x${GOERLI_CHAIN_ID}` }],
+    });
+  }
+
+  const getProof = async () => {
+    const sig = await getSignature();
+    const proof = await fetchProof(sig);
+    await transact(proof);
+  }
+
+  const getSignature = async () => {
+    setmessageSignatureLoading(true);
+    const signature = await library.getSigner(account).signMessage(message);
+    setMessageSignature(signature);
+    setmessageSignatureLoading(false);
+    return signature;
+  }
+
+  const fetchProof = async (signature) => {
+    setApiCallLoading(true);
+    let body = {};
+    try {
+      const res = await fetch(apiUrl(message, signature));
+      setApiCallResponsestatus(res.status);
+      body = await res.json();
+      setApiCallResponseBody(body);
+    } catch(err) {
+      setApiCallError(err);
+    }
+    setApiCallLoading(false);
+    return body;
+  }
+
+  const apiUrl = (message, signature) => {
+    const encMessage = encodeURIComponent(message);
+    return `${BASE_URL}?message=${encMessage}&signature=${signature}`;
+  }
+
+  const transact = async ({
+    proof,
+    validUntil,
+    approvedAt,
+    fractalId,
+  }) => {
+    setTxLoading(true);
+
+    const contract = new Contract(CONTRACT_ADDRESS, contractABI, library?.getSigner());
+    try {
+      const tx = await contract.main(proof, validUntil, approvedAt, fractalId);
+      setTx(tx);
+      await tx.wait();
+      setTxConfirmed(true);
+    } catch(err) {
+      setTxError(err);
+    }
+    setTxLoading(false);
+  }
+
+  const getTransactionURL = () => (
+    `https://goerli.etherscan.io/tx/${tx.hash}`
+  );
+
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        🎉🎉🎉
+    <main>
+      <h1>
+        Action Page
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <div>
+        <p>Connected: {active.toString()}</p>
+        <p>Account: {account}</p>
+        <p>Görli chain: {chainId === GOERLI_CHAIN_ID ? "🟢" : "🔴"}</p>
+        {active && (chainId !== GOERLI_CHAIN_ID) &&
+          <button onClick={switchToGoerliChain}>
+            Switch to Görli chain
+          </button>
+        }
+      </div>
+      {active && account &&
+        <div>
+          <button onClick={getProof}>
+            Make transaction
+          </button>
+          <ol>
+            {(messageSignatureLoading || !!messageSignature) &&
+              <li>Sign message {messageSignatureLoading && "⏱"} {messageSignature && "✅"}
+              {messageSignature && 
+                <pre>
+                  {messageSignature}
+                </pre>
+              }
+              </li>
+            }
+            {(apiCallLoading || apiCallResponseStatus) && 
+              <li>
+                Credentials API call {apiCallLoading && "⏱"} {apiCallResponseStatus && "✅"}
+                {(!apiCallLoading && apiCallResponseStatus) &&
+                  <p>Response status: {apiCallResponseStatus}</p>
+                }
+                {(!apiCallLoading && apiCallResponseBody) && 
+                  <pre>
+                    {JSON.stringify(apiCallResponseBody, null, 2)}
+                  </pre>
+                }
+              </li>
+            }
+            {(txLoading || tx) && 
+              <>
+                <li>
+                  Broadcast transaction {txLoading && !tx && "⏱"} {tx && "✅"}
+                </li>
+                {tx &&
+                  <li>
+                    Waiting for confirmations {txConfirmed? "✅" : "⏱"}
+                    {txConfirmed && 
+                      <p>
+                        <a href={getTransactionURL()} target="_blank">See on Etherscan</a>
+                      </p>
+                    }
+                  </li>
+                }
+              </>
+            }
+          </ol>
+        </div>
+      }
     </main>
   )
 }
 
-export default IndexPage
+export default function() {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <IndexPage />
+    </Web3ReactProvider>   
+  );
+}
